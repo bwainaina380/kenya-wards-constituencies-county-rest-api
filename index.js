665822303c6e74b7de315894851b26d9e3a297f6
bwainaina380 <bwainaina380@gmail.com>
2018-12-17 22:00:31 +0300

    Imported the router into index.js and created a middleware to handle the routes for the different requests

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 
+// Import router from api.js
+const routes = require('./routes/api.js');
+
 // Initialize express app
 const app = express();
+
+// Create middleware to handle routes
+app.use('/api', routes);
 
 
 // Listen for requests on port 3000 or the port allocated by the host such as Heroku

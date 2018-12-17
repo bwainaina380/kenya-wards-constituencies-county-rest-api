e6654687ea450f2d3cdcf9053f3c84c1b417cf75
bwainaina380 <bwainaina380@gmail.com>
2018-12-17 21:56:40 +0300

    Initialized the requests router. Created request handlers for the GET, POST, PUT and DELETE request types

---
+++ routes/api.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+// Initialize router
+const router = express.Router();
+
+// Route to handle GET request
+router.get('/wards', (req, res) => {
+    res.send({type: "GET"});
+});
+
+// Route to handle POST request
+router.post('/wards', (req, res) => {
+    res.send({type: "POST"});
+});
+
+// Route to handle PUT requests
+router.put('/wards/:id', (req, res) => {
+    res.send({type: "PUT"});
+});
+
+// Route to handle DELETE requests
+router.delete('/wards/:id', (req, res) => {
+    res.send({type: "DELETE"});
+});
+
+// Export router
+module.exports = router;

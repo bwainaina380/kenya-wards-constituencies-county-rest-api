56504e6c4e66ba78bf6231d6073718beab19a498
bwainaina380 <bwainaina380@gmail.com>
2019-01-01 14:23:55 +0300

    Added a handler for static files. Created a 'public' folder with an index.html file to act as the API's homepage

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/warddb');
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/warddb");
 
 // Set Mongoose Promise equal to global Promise as Mongoose's Promise is depracated
 mongoose.Promise = global.Promise;
 
 // Import router from api.js
-const routes = require('./routes/api.js');
+const routes = require("./routes/api.js");
 
 // Initialize express app
 const app = express();
 
+app.use(express.static("public"));
+
 // Create custom middleware to handle errors
 app.use((err, req, res, next) => {
-    res.status(403).send({error: err.message});
+  res.status(403).send({ error: err.message });
 });
 
 // Create bodyParser middleware at the top of the stack so that data in request bosy is ready for the routes
 app.use(bodyParser.json());
 
 // Create middleware to handle routes
-app.use('/api', routes);
-
+app.use("/api", routes);
 
 // Listen for requests on port 3000 or the port allocated by the host such as Heroku
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Listening for requests on port 3000");
+  console.log("Listening for requests on port 3000");
 });
